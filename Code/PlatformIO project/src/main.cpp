3b9570aecc10b440c439287c6d492bc7be57c843
XonarDG <44929752+XonarDG@users.noreply.github.com>
2022-12-25 22:18:47 +0100

    Added cathode protection feature

--- Code/PlatformIO project/src/main.cpp
+++ Code/PlatformIO project/src/main.cpp
@@ -22,6 +22,8 @@
 //declaring the RTC
 RTC_DS3231 rtc;
 
+int Minutes = 0;
+int MinutePlusOne = 0;
 int MultiPlexCounter = 0;
 int Space = 0;
 
@@ -91,18 +93,6 @@ void WriteNumber(int number)
   }
 }
 
-void AllNumbers(int delay_length, int seconds)
-{
-
-for (size_t i = 0; i < 9; i++)
-{
-  digitalWrite(TH, HIGH);  
-  WriteNumber(i);
-  delay(delay_length);
-}
-
-}
-
 void setup() 
 {
 
@@ -127,8 +117,80 @@ void setup()
   digitalWrite(C, LOW);
   digitalWrite(D, LOW);
 
+  DateTime time = rtc.now();
+  Minutes = time.minute();
+  if (Minutes < 58)
+  {
+    MinutePlusOne = Minutes + 1;
+  }
+  else
+  {
+    MinutePlusOne = Minutes - 58;
+  }
+  
+
   //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
+}
+
+void AllNumbers(int delay_length, int frequency_in_minutes)
+{
+  int MultiPlexCounter = 0;
+  DateTime time = rtc.now();
+  Minutes = time.minute();
+  if (Minutes == MinutePlusOne)
+  {
+    for (size_t i = 0; i < 4; i++)
+    {
+      for (size_t number = 0; number < 9; number++)
+      { 
+        for (size_t i = 0; i < 50; i++)
+        {
+          switch(MultiPlexCounter)
+          {
+            case 0:
+            digitalWrite(TH, HIGH);
+            WriteNumber(number);
+            MultiPlexCounter++;
+            delay(Space);
+            digitalWrite(TH, LOW);
+            break;
+            case 1:
+            digitalWrite(H, HIGH);
+            WriteNumber(number);
+            MultiPlexCounter++;
+            delay(Space);
+            digitalWrite(H, LOW);
+            break;
+            case 2:
+            digitalWrite(TM, HIGH);
+            WriteNumber(number);
+            MultiPlexCounter++;
+            delay(Space);
+            digitalWrite(TM, LOW);
+            break;
+            case 3:
+            digitalWrite(M, HIGH);
+            WriteNumber(number);
+            MultiPlexCounter = 0;
+            delay(Space);
+            digitalWrite(M, LOW);
+            break;
+          }
+        }
+      }
+    }
+  }
+  if (Minutes < 58)
+  {
+    MinutePlusOne = Minutes + frequency_in_minutes;
+  }
+  else
+  {
+    MinutePlusOne = Minutes - 58;
+  }
+  
+  
 }
 
 void loop()
@@ -153,6 +215,8 @@ void loop()
   Serial.print(TenMinute);
   Serial.print(Minute);
   Serial.println();*/
+
+  AllNumbers(20, 5);
 
   switch(MultiPlexCounter)
   {
