a69bd23d97624b4dc4155fa2c0282d806b4f46f6
XonarDG <vojtik@pinsoft.cz>
2022-12-28 21:55:22 +0100

    Updated code, added and improved methods.
    Adapted the code to use methods only, makes it a bit more clean.

--- Code/PlatformIO project/src/main.cpp
+++ Code/PlatformIO project/src/main.cpp
@@ -25,14 +25,42 @@
 RTC_DS3231 rtc;
 
 bool AllNumbersSwitch = true;
-int MinutesPlus = 0;
-int MultiPlexCounter = 0;
-int Space = 0;
+int unix_time_plus = 0;
+int multiplex_counter = 0;
+int multiplex_timing = 0;
 
-bool Switch = true;
+void setup() 
+{
 
+  //Serial.begin(9600);
+  rtc.begin();
+
+  pinMode(TH, OUTPUT);
+  pinMode(H, OUTPUT);
+  pinMode(TM, OUTPUT);
+  pinMode(M, OUTPUT);
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(colon, OUTPUT);
+
+  digitalWrite(TH, LOW);
+  digitalWrite(H, LOW);
+  digitalWrite(TM, LOW);
+  digitalWrite(M, LOW);
+  digitalWrite(A, LOW);
+  digitalWrite(B, LOW);
+  digitalWrite(C, LOW);
+  digitalWrite(D, LOW);
+  digitalWrite(colon, LOW);
+
+  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+
+}
 void WriteNumber(int number)
 {
+
   switch (number) {
     case 0:
       digitalWrite(A, LOW);
@@ -94,97 +122,57 @@ void WriteNumber(int number)
       digitalWrite(C, LOW);
       digitalWrite(D, LOW);
       break;
+
   }
 }
-
-void setup() 
+void MultiPlex(int first_number, int second_number, int third_number, int forth_number)
 {
 
-  //Serial.begin(9600);
-  rtc.begin();
-
-  pinMode(TH, OUTPUT);
-  pinMode(H, OUTPUT);
-  pinMode(TM, OUTPUT);
-  pinMode(M, OUTPUT);
-  pinMode(A, OUTPUT);
-  pinMode(B, OUTPUT);
-  pinMode(C, OUTPUT);
-  pinMode(D, OUTPUT);
-  pinMode(colon, OUTPUT);
-
-  digitalWrite(TH, LOW);
-  digitalWrite(H, LOW);
-  digitalWrite(TM, LOW);
-  digitalWrite(M, LOW);
-  digitalWrite(A, LOW);
-  digitalWrite(B, LOW);
-  digitalWrite(C, LOW);
-  digitalWrite(D, LOW);
-  digitalWrite(colon, LOW);
-
-  //rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  switch(multiplex_counter)
+  {
+    case 0:
+    digitalWrite(TH, HIGH);
+    WriteNumber(first_number);
+    multiplex_counter++;
+    delay(multiplex_timing);
+    digitalWrite(TH, LOW);
+    break;
+    case 1:
+    digitalWrite(H, HIGH);
+    WriteNumber(second_number);
+    multiplex_counter++;
+    delay(multiplex_timing);
+    digitalWrite(H, LOW);
+    break;
+    case 2:
+    digitalWrite(TM, HIGH);
+    WriteNumber(third_number);
+    multiplex_counter++;
+    delay(multiplex_timing);
+    digitalWrite(TM, LOW);
+    break;
+    case 3:
+    digitalWrite(M, HIGH);
+    WriteNumber(forth_number);
+    multiplex_counter = 0;
+    delay(multiplex_timing);
+    digitalWrite(M, LOW);
+    break;
+  }
 
 }
-void AllNumbers(size_t number_of_repeats, size_t digit_display_time_ms, int frequency_in_minutes, int currentMinute)
+void AllNumbers(size_t number_of_repeats, size_t digit_display_time_ms, int frequency_in_seconds, int unix_time)
 { 
-  int MultiPlexCounter = 0;
-
-  /*int setfrequency = currentMinute % frequency_in_minutes;
-  if (setfrequency == 0)
-  {
-    for (size_t i = 0; i < number_of_repeats; i++)
-    {
-      for (size_t number = 0; number < 9; number++)
-      { 
-        for (size_t i = 0 ; i < Delay; i++)
-        {
-          switch(MultiPlexCounter)
-          {
-            case 0:
-            digitalWrite(TH, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter++;
-            delay(Space);
-            digitalWrite(TH, LOW);
-            break;
-            case 1:
-            digitalWrite(H, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter++;
-            delay(Space);
-            digitalWrite(H, LOW);
-            break;
-            case 2:
-            digitalWrite(TM, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter++;
-            delay(Space);
-            digitalWrite(TM, LOW);
-            break;
-            case 3:
-            digitalWrite(M, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter = 0;
-            delay(Space);
-            digitalWrite(M, LOW);
-            break;
-          }
-        }
-      }
-    }
-  }*/  
-  
   if (AllNumbersSwitch == true)
   {
-     if (MinutesPlus < currentMinute)
+     if (unix_time_plus < unix_time)
     {
-      MinutesPlus = currentMinute + frequency_in_minutes;
+      unix_time_plus = unix_time + frequency_in_seconds;
     }
     AllNumbersSwitch = false;
   }
 
-  if (currentMinute == MinutesPlus)
+  if (unix_time == unix_time_plus)
   {
     for (size_t i = 0; i < number_of_repeats; i++)
     {
@@ -192,167 +180,59 @@ void AllNumbers(size_t number_of_repeats, size_t digit_display_time_ms, int freq
       { 
         for (size_t i = 0 ; i < (digit_display_time_ms / 4); i++)
         {
-          switch(MultiPlexCounter)
-          {
-            case 0:
-            digitalWrite(TH, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter++;
-            delay(Space);
-            digitalWrite(TH, LOW);
-            break;
-            case 1:
-            digitalWrite(H, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter++;
-            delay(Space);
-            digitalWrite(H, LOW);
-            break;
-            case 2:
-            digitalWrite(TM, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter++;
-            delay(Space);
-            digitalWrite(TM, LOW);
-            break;
-            case 3:
-            digitalWrite(M, HIGH);
-            WriteNumber(number);
-            MultiPlexCounter = 0;
-            delay(Space);
-            digitalWrite(M, LOW);
-            break;
-          }
+          MultiPlex(number, number, number, number);
         }
       }
     }
-    MinutesPlus = MinutesPlus + frequency_in_minutes; 
-  }
-    if (MinutesPlus >= 60)
-  {
-    MinutesPlus = MinutesPlus - 60;
+    unix_time_plus = unix_time_plus + frequency_in_seconds; 
   }
 }
-
-void Colon(int currentSecond)
-
+void Colon(int unix_time)
 {
-  int second = currentSecond; 
+
+  int second = unix_time; 
   if (second % 2 == 0)
   {
     digitalWrite(colon, HIGH);
   }
   else
   digitalWrite(colon, LOW);  
-}
 
+}
 void ShowDate(int day, int month, int year, int currentMinute, int frequency_in_minutes, size_t display_time)
 {
 
-MultiPlexCounter = 0;
+  int TenDay = (day / 10) % 10;         
+  int Day = (day % 10);
+  int TenMonth = (month / 10) % 10;
+  int Month = (month % 10);
 
-int TenDay = (day / 10) % 10;         
-int Day = (day % 10);
-int TenMonth = (month / 10) % 10;
-int Month = (month % 10);
- for (size_t i = 0; i < 3600; i++)
- {
-  switch(MultiPlexCounter)
+  for (size_t i = 0; i < 3600; i++)
   {
-    case 0:
-    digitalWrite(TH, HIGH);
-    WriteNumber(TenDay);
-    MultiPlexCounter++;
-    delay(Space);
-    digitalWrite(TH, LOW);
-    break;
-    case 1:
-    digitalWrite(H, HIGH);
-    WriteNumber(Day);
-    MultiPlexCounter++;
-    delay(Space);
-    digitalWrite(H, LOW);
-    break;
-    case 2:
-    digitalWrite(TM, HIGH);
-    WriteNumber(TenMonth);
-    MultiPlexCounter++;
-    delay(Space);
-    digitalWrite(TM, LOW);
-    break;
-    case 3:
-    digitalWrite(M, HIGH);
-    WriteNumber(Month);
-    MultiPlexCounter = 0;
-    delay(Space);
-    digitalWrite(M, LOW);
-    break;
-    }
- }
- 
-  
+    MultiPlex(TenDay, Day, TenMonth, Month);
   }
 
+}
+void ShowTime(int hour, int minute)
+{
+
+  int TenHour = (hour / 10) % 10;         
+  int Hour = (hour % 10);
+  int TenMinute = (minute / 10) % 10;
+  int Minute = (minute % 10);
+
+  MultiPlex(TenHour, Hour, TenMinute, Minute);
+
+}
 void loop()
 {
 
-  Space = 1;
+  multiplex_timing = 1;
 
   DateTime time = rtc.now();
 
-  int TenHour = 0;
-  int Hour = 0;
-  int TenMinute = 0;
-  int Minute = 0;
-
-  TenHour = (time.hour() / 10) % 10;         
-  Hour = (time.hour() % 10);
-  TenMinute = (time.minute() / 10) % 10;
-  Minute = (time.minute() % 10);
-
-  /*Serial.print(TenHour);
-  Serial.print(Hour);
-  Serial.print(TenMinute);
-  Serial.print(Minute);
-  Serial.println();*/
-    if (Switch == true)
-  {
-    ShowDate(time.day(), time.month(), time.year(), time.minute(), 30, 300);
-    Switch = false;
-  }
   AllNumbers(3, 200, 7, time.minute());
   Colon(time.second());
+  ShowTime(time.hour(), time.minute());
 
-  switch(MultiPlexCounter)
-  {
-    case 0:
-    digitalWrite(TH, HIGH);
-    WriteNumber(TenHour);
-    MultiPlexCounter++;
-    delay(Space);
-    digitalWrite(TH, LOW);
-    break;
-    case 1:
-    digitalWrite(H, HIGH);
-    WriteNumber(Hour);
-    MultiPlexCounter++;
-    delay(Space);
-    digitalWrite(H, LOW);
-    break;
-    case 2:
-    digitalWrite(TM, HIGH);
-    WriteNumber(TenMinute);
-    MultiPlexCounter++;
-    delay(Space);
-    digitalWrite(TM, LOW);
-    break;
-    case 3:
-    digitalWrite(M, HIGH);
-    WriteNumber(Minute);
-    MultiPlexCounter = 0;
-    delay(Space);
-    digitalWrite(M, LOW);
-    break;
-  }
-  //AllNumbers(10, 10);
 }
