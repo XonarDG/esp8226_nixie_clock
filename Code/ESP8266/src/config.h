3113dbeece82349df99b7f66e8c94f40ec5c9530
XonarDG <vojtik@pinsoft.cz>
2024-03-28 10:28:16 +0100

    Code and PCB update

--- Code/ESP8266/src/config.h
+++ Code/ESP8266/src/config.h
@@ -1,10 +1,15 @@
-#define SSID "OrionC_1" //Kako
-#define PASS "Xr4q7PkW25**22"         //dmmq9361
+#define SSID "OrionC_1"         //Kako
+#define PASS "Xr4q7PkW25**22"   //dmmq9361
 
-int animation_interval_sec = random(20, 60);
+static long gmtoffset_sec = 3600; 
+static int daylightoffset_sec = 0;
+static const char* ntpserver = "0.cz.pool.ntp.org";
+
+int animation_interval_sec = random(60, 240);
 u_int animation_next;
 
 int ntp_update_interval_sec = 43200;
+int ntp_update_interval_failed = 240;
 u_int ntp_update_next;
 
 int date_interval_sec;
--- Code/ESP8266/src/main.cpp
+++ Code/ESP8266/src/main.cpp
@@ -1,30 +1,26 @@
 #include <Arduino.h>
-#include <WiFiUdp.h>
 #include <ESP8266WiFi.h>
 #include <RTClib.h>
 #include <SPI.h>
 
 #include "config.h"
 
-#define DATA 14
-#define CLOCK 12
-#define LATCH 13
+#define DATA D5
+#define CLOCK D7
+#define LATCH D6
 
-#define SDA 5
-#define SCL 4
+#define SDA D1
+#define SCL D2
 
 //configuration variables for NTP
-long gmtoffset_sec = 3600; 
-int daylightoffset_sec = 0;
-const char* ntpserver = "0.cz.pool.ntp.org";
 
-WiFiUDP ntpUDP;
+
 RTC_DS3231 ds3231;
 
 //integer arrays used as a truth table for the shift registers
-int number_arr[] = {512,1,2,4,8,16,32,64,128,256};
-int display_arr[] = {1,2,4,8,16,32};
-int colon_arr[] = {1,2,4,8};
+int cathode_arr[] = {512,1,2,4,8,16,32,64,128,256};
+int anode_arr[] = {1,2,4,8,16,32};
+int colon_arr[] = {0b0000000, 0b00000011, 0b00001100,0b00001010, 0b00000101, 0b00000011, 0b00001100, 0b00001010, 0b00001111};
 
 //shift register data variables
 uint8_t shift_reg1;
@@ -32,99 +28,84 @@ uint8_t shift_reg2;
 uint8_t shift_reg3;
 uint8_t shift_reg_null;
 
+//connects to Wi-Fi
 void WiFiBegin()
 {
   WiFi.begin(SSID, PASS);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    for (size_t i = 0; i < 3; i++)
-    {
-      delay (500);
-    }
-  }
-  //NTPC.begin();
 }
-void WiFiStop()
-{
-  WiFi.disconnect();
-  WiFi.mode(WIFI_OFF);
-}
-
+//defines custom I2C pins, establishes I2C comms with RTC
 void RTCBegin()
 {
   Wire.begin(SDA, SCL);
   ds3231.begin();
 }
-void RTCSync(DateTime time)
+//gets NTP time and returns it in DateTime object
+DateTime GetNTPTime()
 {
-  ds3231.adjust(time);
-}
-DateTime NTPSync()
-{
-  WiFiBegin();
-  struct tm timeinfo;
+  struct tm ntp_time;
   configTime(gmtoffset_sec, daylightoffset_sec, ntpserver);
-  getLocalTime(&timeinfo);
-  DateTime time = DateTime(timeinfo.tm_year+1900, timeinfo.tm_mon+1, timeinfo.tm_mday, timeinfo.tm_hour, timeinfo.tm_min, timeinfo.tm_sec);
+  getLocalTime(&ntp_time);
+  DateTime time = DateTime(ntp_time.tm_year+1900, ntp_time.tm_mon+1, ntp_time.tm_mday, ntp_time.tm_hour, ntp_time.tm_min, ntp_time.tm_sec);
   return time;
 }
+//connects to Wi-Fi, gets NTP time, updates RTC, disconnects from Wi-Fi
 void TimeSync()
 {
-  WiFiBegin();
-  RTCSync(NTPSync());
-  WiFiStop();
+  ds3231.adjust(GetNTPTime());
+  WiFi.disconnect();
+  WiFi.mode(WIFI_OFF);
 }
-
+//handles data sent to shift register
 void SendData(uint8_t data1, uint8_t data2, uint8_t data3)
 {
-  digitalWrite(LATCH, LOW);
-  shiftOut(DATA, CLOCK, MSBFIRST, data1);
-  shiftOut(DATA, CLOCK, MSBFIRST, data2);
+  digitalWrite(LATCH, LOW);               //sets the LATCH pin LOW, allows shift registers to read data
+  shiftOut(DATA, CLOCK, MSBFIRST, data1); //sends first 8 bits
+  shiftOut(DATA, CLOCK, MSBFIRST, data2); 
   shiftOut(DATA, CLOCK, MSBFIRST, data3);
-  digitalWrite(LATCH, HIGH);
+  digitalWrite(LATCH, HIGH);              //sets the LATCH pin HIGH, shift registers now act as memory
 }
-void Multiplex(int hh, int h, int mm, int m, int ss, int s, int colons, int multiplex_delay_micro, int blank_delay_micro)
+//multiplexes given data
+void Multiplex(int hh, int h, int mm, int m, int ss, int s, int multiplex_delay_micro, int blank_delay_micro)
 {
   int time_arr[] = {hh,h,mm,m,ss,s};
 
   for (size_t i = 0; i <= 5; i++)
   {
-  int cathodes = time_arr[i];
-  int set_colons = colons;
-
-  int shown_number = number_arr[cathodes];
-  int shown_colons = colon_arr[set_colons];
-
-  shift_reg1 = (shown_number >> 2); 
-  shift_reg2 = ((shown_number << 6) | (display_arr[i]));
-  shift_reg3 = shown_colons;
-
-  shift_reg_null = ((shown_number << 6) | 0);
-  SendData(shift_reg1, shift_reg2, shift_reg3);
-  delayMicroseconds(multiplex_delay_micro);
-  SendData(shift_reg1, shift_reg_null, shift_reg3);
-  delayMicroseconds(blank_delay_micro);
-  }
-}
-void Multiplex(int arr[6], int colons, int multiplex_delay_micro, int blank_delay_micro)
-{
-  for (size_t i = 0; i <= 5; i++)
+  
+  //simple logic to blink the colons on even numbers
+  int set_colons;
+  if (WiFi.status() == WL_NO_SSID_AVAIL)
   {
-  int cathodes = arr[i];
-  int set_colons = colons;
+  if (s % 2 == 1)
+  set_colons = 0;
+  else
+  set_colons = 7;
+  }
+  else
+  {
+  if (s % 2 == 1)
+  set_colons = 0;
+  else
+  set_colons = 8;
+  }
+  
+  //sets the cathodes according to the multiplex cycle
+  int cathodes = time_arr[i];
+  int cathode_state = cathode_arr[cathodes];
 
-  int shown_number = number_arr[cathodes];
-  int shown_colons = colon_arr[set_colons];
+  //bit manipulation to give each shift register correct bits
+  shift_reg1 = (cathode_state >> 2);                       //takes the first 8 bits from cathode_state        
+  shift_reg2 = ((cathode_state << 6) | (anode_arr[i]));  //takes the last 2 bits cathode_state and all six from display_arr(which is set by the current multiplex cycle)
+  shift_reg3 = colon_arr[set_colons];
 
-  shift_reg1 = (shown_number >> 2); 
-  shift_reg2 = ((shown_number << 6) | (display_arr[i]));
-  shift_reg3 = shown_colons;
+  //setting 
+  shift_reg_null = ((cathode_state << 6) | 0);
 
-  shift_reg_null = ((shown_number << 6) | 0);
-  SendData(shift_reg1, shift_reg2, shift_reg3);
-  delayMicroseconds(multiplex_delay_micro);
-  SendData(shift_reg1, shift_reg_null, shift_reg3);
-  delayMicroseconds(blank_delay_micro);
+  //send data with blanking interval
+  SendData(shift_reg1, shift_reg2, shift_reg3);           
+  delayMicroseconds(multiplex_delay_micro);             
+  SendData(shift_reg1, shift_reg_null, colon_arr[0]);   //blanking data
+  delayMicroseconds(blank_delay_micro);                 //length of blanking interval
   }
 }
 void Animation(u_long speed, u_int repeats)
@@ -136,7 +117,7 @@ void Animation(u_long speed, u_int repeats)
     for (size_t i = 0; i < 10;)
     {
       current_millis = millis();
-      Multiplex(i,i,i,i,i,i, colon_arr[0], 1000, 500);
+      Multiplex(i,i,i,i,i,i, 1000, 500);
       yield();
       if (current_millis - start_millis >= speed)
       {
@@ -149,18 +130,22 @@ void Animation(u_long speed, u_int repeats)
 
 void setup() 
 {
-  pinMode(DATA, OUTPUT);
+  //declares pinmodes
+  pinMode(DATA, OUTPUT);    
   pinMode(CLOCK, OUTPUT);
   pinMode(LATCH, OUTPUT);
-  digitalWrite(DATA,LOW);
+
+  //sets all pins LOW
+  digitalWrite(DATA,LOW);   
   digitalWrite(CLOCK,LOW);
   digitalWrite(LATCH,LOW);
 
-  WiFi.setAutoReconnect(false);
-  SendData(0,0,0);
-  RTCBegin();
-  TimeSync();
+  WiFi.setAutoReconnect(false); 
+  SendData(0,0,0);  //sends empty data
+  RTCBegin();       //starts I2C session with the RTC module
+  WiFiBegin();
 
+  //sets time of next trigger for timed methods
   DateTime rtc = ds3231.now();
   int unixtime = rtc.unixtime();
   animation_next = unixtime + animation_interval_sec;
@@ -169,17 +154,7 @@ void setup()
 
 void loop()
 {
-  //NTPC.update();
   DateTime rtc = ds3231.now();
-
-  /*
-  int hh = (NTPC.getHours() / 10) % 10;         
-  int h = (NTPC.getHours() % 10);
-  int mm = (NTPC.getMinutes() / 10) % 10;
-  int m = (NTPC.getMinutes() % 10);
-  int ss = (NTPC.getSeconds() / 10) % 10;
-  int s = (NTPC.getSeconds() % 10);
-  */
 
   int hh = (rtc.hour() / 10) % 10;         
   int h = (rtc.hour() % 10);
@@ -187,7 +162,7 @@ void loop()
   int m = (rtc.minute() % 10);
   int ss = (rtc.second() / 10) % 10;
   int s = (rtc.second() % 10);
-  Multiplex(hh,h,mm,m,ss,s, colon_arr[1], 1000, 500);
+  Multiplex(hh,h,mm,m,ss,s, 1000, 500);
   
   if (rtc.unixtime() >= animation_next)
   {
@@ -197,7 +172,11 @@ void loop()
   
   if (rtc.unixtime() >= ntp_update_next)
   {
-    TimeSync();
+    WiFiBegin();
     ntp_update_next = rtc.unixtime() + ntp_update_interval_sec;
+  }
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    TimeSync();
   }
 }
